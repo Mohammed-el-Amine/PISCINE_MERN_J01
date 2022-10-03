9a1456ac9e0ee58344560fcc1fbfb81fc4ade2ce
amine <mohammed-el-amine.djellal@epitech.eu>
2022-10-03 12:37:18 +0200

    [ADD] render file with dynamic name

---
+++ ex_04/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 4242;
+
+// Set EJS as templating engine 
+app.set('view engine', 'ejs');
+
+// sendFile resend to unknow user page
+app.get('/user/', function (req, res, next) {
+    res.sendFile(path.join(__dirname, '/views/hello_unknow.html'));
+});
+
+//sendFile resend to name with user page
+app.get('/user/:name', function (req, res, next) {
+    const name = req.params.name ;
+    res.render('hello',{text: name});
+});
+
+//config message + port
+app.listen(port, () => {
+    console.log('Server app listening on port ' + port);
+});
