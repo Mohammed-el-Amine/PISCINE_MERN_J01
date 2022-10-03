05bd3ee3f667bbcd9fd33e3d8b4b30ff405b1f5c
amine <mohammed-el-amine.djellal@epitech.eu>
2022-10-03 16:47:35 +0200

    [FIX] update in module format

--- ex_06/myMERN_module.js
+++ ex_06/myMERN_module.js
@@ -1,40 +1,34 @@
-function create(name){
-    const fs = require('fs');
-    fs.open(`${name}`, 'w', function (err,file) {
-        if (err) throw console.log(`Create ${name} : KO`);
-        console.log(`Create ${name} : OK`);
-    });
-};
-
-function read(name){
-    const fs = require('fs')
-    fs.readFile(`${name}`, 'utf8' , (err, data) => {
-        if (err) {
-        console.log(`Read ${name} : KO`);
-        return
-        }
-        console.log(data)
-    })
-};
-
-function update(name, content){
-    var fs = require('fs');
-    fs.writeFile(`${name}`, `${content}`, function (err) {
-    if (err) throw console.log(`Update ${name} : KO`);
-    console.log(`Update ${name} : OK`);
-    });
-};
-
-function deleted(name){
-    var fs = require('fs');
-    fs.unlink(`${name}`, function (err) {
-    if (err) throw console.log(`Delete ${name} : KO`);
-    console.log(`Delete ${name} : OK`);
-    });
-};
-
-
-//create("hoho"); //fonctionne correctement
-//read("hoho");  // fonctionne correctement
-//update("ho","je suis le remplacement");  // fonctionne correctement
-//deleted("hoho"); // fonctionne correctement
+myMERN = {
+    create: function (name) {
+        const fs = require('fs');
+        fs.open(`${name}`, 'w', function (err, file) {
+            if (err) throw console.log(`Create ${name} : KO`);
+            console.log(`Create ${name} : OK`);
+        });
+    },
+    read: function (name) {
+        const fs = require('fs')
+        fs.readFile(`${name}`, 'utf8', (err, data) => {
+            if (err) {
+                console.log(`Read ${name} : KO`);
+                return
+            }
+            console.log(data)
+        })
+    },
+    update: function (name, content) {
+        var fs = require('fs');
+        fs.writeFile(`${name}`, `${content}`, function (err) {
+            if (err) throw console.log(`Update ${name} : KO`);
+            console.log(`Update ${name} : OK`);
+        });
+    },
+    delete:function (name) {
+        var fs = require('fs');
+        fs.unlink(`${name}`, function (err) {
+            if (err) throw console.log(`Delete ${name} : KO`);
+            console.log(`Delete ${name} : OK`);
+        });
+    }
+}
+module.exports = myMERN;
