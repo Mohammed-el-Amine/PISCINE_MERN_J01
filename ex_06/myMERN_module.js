e6d1096bd1f6f2626195efd854d6f2240513d04f
amine <mohammed-el-amine.djellal@epitech.eu>
2022-10-03 16:23:10 +0200

    [UPDATE] repalce console.error by console.log

--- ex_06/myMERN_module.js
+++ ex_06/myMERN_module.js
@@ -1,7 +1,7 @@
 function create(name){
     const fs = require('fs');
     fs.open(`${name}`, 'w', function (err,file) {
-        if (err) throw console.error(`Create ${name} : KO`);
+        if (err) throw console.log(`Create ${name} : KO`);
         console.log(`Create ${name} : OK`);
     });
 };
@@ -10,7 +10,7 @@ function read(name){
     const fs = require('fs')
     fs.readFile(`${name}`, 'utf8' , (err, data) => {
         if (err) {
-        console.error(`Read ${name} : KO`);
+        console.log(`Read ${name} : KO`);
         return
         }
         console.log(data)
@@ -20,7 +20,7 @@ function read(name){
 function update(name, content){
     var fs = require('fs');
     fs.writeFile(`${name}`, `${content}`, function (err) {
-    if (err) throw console.error(`Update ${name} : KO`);
+    if (err) throw console.log(`Update ${name} : KO`);
     console.log(`Update ${name} : OK`);
     });
 };
@@ -28,7 +28,7 @@ function update(name, content){
 function deleted(name){
     var fs = require('fs');
     fs.unlink(`${name}`, function (err) {
-    if (err) throw console.error(`Delete ${name} : KO`);
+    if (err) throw console.log(`Delete ${name} : KO`);
     console.log(`Delete ${name} : OK`);
     });
 };
