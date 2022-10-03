a9c6f062fa0f0d8908aa4daf13e15bc890fba1a2
amine <mohammed-el-amine.djellal@epitech.eu>
2022-10-03 16:18:53 +0200

    [UPDATE] all function are good update/deleted/create/read

--- ex_06/myMERN_module.js
+++ ex_06/myMERN_module.js
@@ -1,10 +1,40 @@
-function create(name) {
-    var fs = require('fs');
-    fs.open(`${name}`, 'w', function (err,
-        file) {
-        if (err) throw console.log(`Create ${name} : KO`);
+function create(name){
+    const fs = require('fs');
+    fs.open(`${name}`, 'w', function (err,file) {
+        if (err) throw console.error(`Create ${name} : KO`);
         console.log(`Create ${name} : OK`);
     });
 };
 
-//create("hoho"); fonctionne correctement
+function read(name){
+    const fs = require('fs')
+    fs.readFile(`${name}`, 'utf8' , (err, data) => {
+        if (err) {
+        console.error(`Read ${name} : KO`);
+        return
+        }
+        console.log(data)
+    })
+};
+
+function update(name, content){
+    var fs = require('fs');
+    fs.writeFile(`${name}`, `${content}`, function (err) {
+    if (err) throw console.error(`Update ${name} : KO`);
+    console.log(`Update ${name} : OK`);
+    });
+};
+
+function deleted(name){
+    var fs = require('fs');
+    fs.unlink(`${name}`, function (err) {
+    if (err) throw console.error(`Delete ${name} : KO`);
+    console.log(`Delete ${name} : OK`);
+    });
+};
+
+
+//create("hoho"); //fonctionne correctement
+//read("hoho");  // fonctionne correctement
+//update("ho","je suis le remplacement");  // fonctionne correctement
+//deleted("hoho"); // fonctionne correctement
